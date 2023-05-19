46f27269503b0578bf75d3a0f452d8ac7c3e17a4
jvictorlopez <124679867+jvictorlopez@users.noreply.github.com>
2023-05-18 21:39:28 -0300

    enviando arquivos do projeto - pasta ''gmane''

---
+++ gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','indiana.edu','berkeley.edu','ufp.pt','uct.ac.za','columbia.edu','gmail.com','etudes.org','mac.com'],
+['2005-12',57,11,12,12,10,14,13,10,5,12],
+['2006-01',114,37,32,33,32,27,28,23,29,14],
+['2006-02',4,0,2,1,3,2,1,3,1,5]
+];
